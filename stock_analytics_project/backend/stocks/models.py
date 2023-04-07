feb38b30cafe9a556002eac18e143935d18d6369
ikechukwueze <themedric@live.com>
2023-04-07 23:38:39 +0100

    create stock exchange model

--- stock_analytics_project/backend/stocks/models.py
+++ stock_analytics_project/backend/stocks/models.py
@@ -4,4 +4,16 @@ from django.db import models
 
 
 
+class StockExchange(models.Model):
+    name = models.CharField(max_length=100)
+    acronym = models.CharField(max_length=30)
+    mic = models.CharField(max_length=15)
+    country = models.CharField(max_length=100)
+    country_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=100)
+    website = models.CharField(max_length=50)
+    timezone = models.CharField(max_length=50)
+    timezone_abbr = models.CharField(max_length=20)
 
+    def __str__(self) -> str:
+        return self.acronym
