c23695a355cd54ccf7ce3af3335830f8e2e2af91
ikechukwueze <themedric@live.com>
2023-04-09 12:04:50 +0100

    create admin

--- stock_analytics_project/backend/stocks/admin.py
+++ stock_analytics_project/backend/stocks/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import StockExchange, StockPrice, Ticker
 
 # Register your models here.
+
+
+class StockExchangeAdmin(admin.ModelAdmin):
+    list_display = ["name", "acronym"]
+    search_fields = ["name", "acronym"]
+
+
+class StockPriceAdmin(admin.ModelAdmin):
+    list_display = ["symbol", "open", "close", "high", "low", "volume"]
+    search_fields = ["symbol"]
+
+
+class TickerAdmin(admin.ModelAdmin):
+    list_display = ["name", "symbol"]
+    search_fields = ["name", "symbol"]
+    list_filter = ["exchange"]
+
+
+
+
+admin.site.register(StockExchange, StockExchangeAdmin)
+admin.site.register(StockPrice, StockPriceAdmin)
+admin.site.register(Ticker, TickerAdmin)
