20c9a4d9f4a8f62e122a50d033f3b81d8a2de264
ikechukwueze <themedric@live.com>
2023-04-16 22:44:23 +0100

    update

--- stock_analytics_project/backend/stocks/filters.py
+++ stock_analytics_project/backend/stocks/filters.py
@@ -5,6 +5,7 @@ from .models import StockPrice
 class StockPriceFilter(filters.FilterSet):
     date = filters.DateFilter(field_name='date')
     date_from = filters.DateFilter(field_name='date', lookup_expr='gte')
+    date_to = filters.DateFilter(field_name='date', lookup_expr='lte')
 
     class Meta:
         model = StockPrice
