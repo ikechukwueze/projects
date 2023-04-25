d6de238d04ade164359af30b76bd6bb1da54d752
ikechukwueze <themedric@live.com>
2023-04-25 23:19:06 +0100

    update url

--- stock_analytics_project/backend/stocks/urls.py
+++ stock_analytics_project/backend/stocks/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('stocks/', views.ListStocks.as_view()),
     path('exchanges/', views.ListExchanges.as_view()),
-    path('portfolio/historical-data/<symbol>/<mic>/', views.HistoricalData.as_view()),
+    path('historical-data/', views.HistoricalData.as_view()),
+    path('portfolio/', views.ListPortfolio.as_view()),
     path('portfolio/add/', views.AddStocktoPorfolioView.as_view())
 ]
