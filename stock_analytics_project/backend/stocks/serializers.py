628236b26e7f6b1304a371d0669b31465a6436c8
ikechukwueze <themedric@live.com>
2023-04-25 23:24:33 +0100

    add stock to portfolio serializer

--- stock_analytics_project/backend/stocks/serializers.py
+++ stock_analytics_project/backend/stocks/serializers.py
@@ -58,3 +58,33 @@ class HistoricalDataSerializer(serializers.Serializer):
             return attrs
 
 
+class AddStocktoPortfolioSerializer(serializers.Serializer):
+    name = serializers.CharField(required=True)
+    symbol = serializers.ChoiceField(
+        choices=[(symbol, symbol) for symbol in Stock.objects.values_list("symbol").distinct()]
+    )
+    mic = serializers.ChoiceField(
+        choices=[(mic, mic) for mic in StockExchange.objects.values_list("mic")]
+    )
+
+    def validate_name(self, value):
+        if not StockPortfolio.objects.filter(name=value).exists():
+            raise serializers.ValidationError({"error": f"{value} portfolio does not exist"})
+        return value
+
+    def validate(self, attrs):
+        symbol = attrs["symbol"]
+        mic = attrs["mic"]
+
+        if not Stock.objects.filter(symbol=symbol, exchange__mic=mic).exists():
+            raise serializers.ValidationError({"error": f"{symbol} is not registered on {mic}"})
+        return attrs
+    
+    def save(self):
+        symbol = self.validated_data["symbol"]
+        mic = self.validated_data["mic"]
+        portfolio_name = self.validated_data["name"]
+        owner = self.context["owner"]
+        stock = Stock.objects.get(symbol=symbol, exchange__mic=mic)
+        portfolio = StockPortfolio.objects.get(owner=owner, name=portfolio_name)
+        portfolio.stocks.add(stock)
