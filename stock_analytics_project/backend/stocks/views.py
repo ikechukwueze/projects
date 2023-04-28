b76e69e09229f235c4b083170ee8c31e18288ee2
ikechukwueze <themedric@live.com>
2023-04-28 19:21:01 +0100

    create views

--- stock_analytics_project/backend/stocks/views.py
+++ stock_analytics_project/backend/stocks/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import views
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import StockPrice, Stock, StockExchange, StockPortfolio
+from .serializers import (
+    StockPriceSerializer,
+    SimpleStockSerializer,
+    StockExchangeSerializer,
+    StockPortfolioSerializer,
+    HistoricalDataSerializer,
+    AddStocktoPortfolioSerializer,
+)
+from .filters import StockPriceFilter
 
 # Create your views here.
+
+
+class ListStocks(generics.ListAPIView):
+    serializer_class = SimpleStockSerializer
+    queryset = Stock.objects.all()
+
+
+
